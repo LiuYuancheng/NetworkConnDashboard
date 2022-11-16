f1c2360ab15af75e5bdd4767e4fd0b335db539c7
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2022-11-16 17:41:09 +0800

    Added the feature to link to the telegram bot to report to the group chat.

--- src/pingClient.py
+++ src/pingClient.py
@@ -16,9 +16,13 @@
 import os
 import time
 from pythonping import ping
-
+import requests
+from datetime import datetime
+from statistics import mean
 import udpCom
 import Log
+
+
 
 # Config the local data/log storage folder
 print("Current working directory is : %s" % os.getcwd())
@@ -54,6 +58,20 @@ ipAdrrDict = {
     'Bbc_co_uk':'BBC.CO.UK'
 }
 
+pingRst = {
+    'Google': [],
+    'CR1': [],
+    'Sutd': [],
+    'Singtel': [],
+    'Gov_sg': [],
+    'Bbc_co_uk': [],
+}
+
+count = 5 # used to count the time 
+
+BOT_TOKEN = "refer to the file <pingClientRun.py>"
+CHAT_ID = "-1001547453974"
+
 # BBC.CO.UK
 # 172.16.1.10 (vcentre)
 # 172.18.178.6 
@@ -70,9 +88,38 @@ while True:
         data = ping(val, timeout=1, verbose=False)
         print(" Peer [%s] ping min: %s ms, avg: %s ms, max: %s ms" % (key, str(data.rtt_min_ms),  str(data.rtt_avg_ms), str(data.rtt_max_ms)))
         Log.info('[%s]: min:%s,avg:%s,max:%s', key, str(data.rtt_min_ms), str(data.rtt_avg_ms), str(data.rtt_max_ms))
+        pingRst[key].append(data.rtt_avg_ms)
         msg = ';'.join((key, str(data.rtt_min_ms), str(data.rtt_avg_ms), str(data.rtt_max_ms)))
         resp = iConnector.sendMsg(msg, resp=False)
-        time.sleep(3)
+        time.sleep(5)
+    count -= 1
+    # call telegram API to message the reuslt to group 
+    if count == 0:
+        timeStr = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        msg = 'NCL-MPH Connection HUB report: \n'
+        msg+= 'Time: %s \n' %timeStr
+        msg+= 'During passed 5 min each peers avg ping are: \n'
+        for item in pingRst.items(): 
+            key, val = item
+            msg += str(' - '+ key +' : '+ str( round(mean(val),3))+' ms \n')
+        try:
+            url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={msg}"
+            requests.get(url).json()
+            pass 
+        except Exception as e:
+            Log.error('report to telegram error:')
+            Log.info(str(e))
+            pass
+        # reset the reault:
+        pingRst = {
+            'Google': [],
+            'CR1': [],
+            'Sutd': [],
+            'Singtel': [],
+            'Gov_sg': [],
+            'Bbc_co_uk': [],
+        }
+        count = 6
     time.sleep(30)
 
 Log.info("Finished the ping test.")
